9a6a9275deba6df1145feacf7f39d71f8be55b6a
murderteeth <murdertxxth@gmail.com>
2023-06-14 06:42:13 +0000

    Check bash commands for security threats

--- ai.ts
+++ ai.ts
@@ -1,12 +1,32 @@
-import { ChatCompletionRequestMessage, Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai'
+import { ChatCompletionRequestMessage, ChatCompletionRequestMessageFunctionCall, Configuration, CreateChatCompletionResponse, OpenAIApi } from 'openai'
 import { AxiosResponse } from 'axios'
 
-export const DEFAULT_MODEL = 'gpt-3.5-turbo'
-export const STRONGEST_MODEL = 'gpt-4'
+export const DEFAULT_MODEL = 'gpt-3.5-turbo-0613'
+export const STRONGEST_MODEL = 'gpt-4-0613'
 export const MODELS = [DEFAULT_MODEL, STRONGEST_MODEL]
-export type Model = 'gpt-4' | 'gpt-3.5-turbo';
+export type Model = 'gpt-4-0613' | 'gpt-3.5-turbo-0613';
 
-export async function one_shot(prompt: string, model: Model = DEFAULT_MODEL, temperature = 0.4) {
+export interface GptFunction {
+  name: string
+  description: string
+  parameters: {
+    type: 'object' | 'string'
+    properties: {
+      [key: string]: {
+        type: 'string'
+        description: string
+        enum?: string[]
+      }
+    }
+  }
+}
+
+export async function one_shot(
+  prompt: string, 
+  functions?: GptFunction[] | null | undefined,
+  model: Model = DEFAULT_MODEL, 
+  temperature = 0.4
+) {
   const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }))
 
   if(process.env.NODE_ENV === 'development') {
@@ -20,6 +40,7 @@ export async function one_shot(prompt: string, model: Model = DEFAULT_MODEL, tem
 
   const result = await openai.createChatCompletion({
     messages: [{ role: 'user', content: prompt }], 
+    ...(functions ? { functions } : {}),
     model: model,
     temperature
   })
@@ -27,7 +48,12 @@ export async function one_shot(prompt: string, model: Model = DEFAULT_MODEL, tem
   return top_choice(result as AxiosResponse<CreateChatCompletionResponse, any>)
 }
 
-export async function next_message(messages: ChatCompletionRequestMessage[], model: Model = DEFAULT_MODEL, temperature = 0.4) {
+export async function next_message(
+  messages: ChatCompletionRequestMessage[], 
+  functions?: GptFunction[] | null | undefined,
+  model: Model = DEFAULT_MODEL,
+  temperature = 0.4
+) {
   const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }))
 
   if(process.env.NODE_ENV === 'development') {
@@ -44,6 +70,7 @@ export async function next_message(messages: ChatCompletionRequestMessage[], mod
   const result = await openai.createChatCompletion({
     messages,
     model: model,
+    ...(functions ? { functions } : {}),
     temperature
   })
 
@@ -53,8 +80,9 @@ export async function next_message(messages: ChatCompletionRequestMessage[], mod
 export function top_choice(response: AxiosResponse<CreateChatCompletionResponse, any>) {
   if(!response.data.choices.length) throw '!choices'
   const content = response.data.choices[0].message?.content
-  if(!content) throw '!content'
-  return content
+  const function_call = response.data.choices[0].message?.function_call
+  if(!(content || function_call)) throw '!(content || function_call)'
+  return content || (function_call as ChatCompletionRequestMessageFunctionCall)
 }
 
 export async function moderated(user_prompt: string) {
--- index.ts
+++ index.ts
@@ -12,19 +12,48 @@ import prompts from 'prompts'
 
 const execPromise = util.promisify(exec)
 
-const bash_prompt = {
-  system: oneLine`you are a savant at bash commands.
+const exitCommands = ['quit', 'q', 'exit']
+
+const ai_prompts = {
+  bash_system: oneLine`you are a savant at bash commands.
 you only know bash commands, you don't know anything else.
 translate USER's input into a bash command.
 if you're not sure how to translate USER's input, say "IDK".
-you can only say bash commands or "IDK".`
+you can only say bash commands or "IDK".`,
+
+  security_system: oneLine`you are a savant at linux security
+you only know linux security, you don't know anything else.
+USER will input a bash command.
+evaluate USER's command for security threats.
+report the results of your security check by calling report_security_check.
+you can only respond in json format.
+
+FORMAT:
+{
+  "severity": "none" | "low" | "medium" | "high" | "critical",
+  "summary": "a very brief, one sentance, summary of the security check result"
+}
+`
+}
+
+interface SecurityCheck {
+  severity: 'none' | 'low' | 'medium' | 'high' | 'critical'
+  summary: string
 }
 
 async function toBash(input: string) {
   return await next_message([
-    { role: 'system', content: bash_prompt.system },
+    { role: 'system', content: ai_prompts.bash_system },
     { role: 'user', content: input }
-  ], STRONGEST_MODEL, 0)
+  ], null, STRONGEST_MODEL, 0)
+}
+
+async function securityCheck(input: string) {
+  const result = await next_message([
+    { role: 'system', content: ai_prompts.security_system },
+    { role: 'user', content: input }
+  ], null, STRONGEST_MODEL, 0) as string
+  return JSON.parse(result) as SecurityCheck
 }
 
 (async () => {
@@ -47,20 +76,38 @@ async function toBash(input: string) {
     if (line === '') {
       rl.close()
       continue
-    } else if (line === 'quit' || line === 'q') {
+    } else if (exitCommands.includes(line.toLowerCase())) {
       rl.close()
       process.exit()
     } else {
       const spinner = new Spinner()
       spinner.setSpinnerString(14)
       spinner.start()
-      const bash = await toBash(line)
+      const bash = await toBash(line) as string
+      const check = await securityCheck(bash)
       spinner.stop(true)
 
       if(bash === 'IDK') {
         console.log(chalk.green('Aint that some 💩 try again'))
         rl.close()
         continue
+      }
+
+      if(check.severity === 'critical') {
+        console.log()
+        console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
+        console.log(chalk.red(check.summary))
+        console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
+        console.log()
+        rl.close()
+        continue
+      }
+
+      if(check.severity !== 'none') {
+        console.log()
+        console.log(chalk.yellow(`THREAT LEVEL: ${check.severity}`))
+        console.log(chalk.yellow(check.summary))
+        console.log()
       }
 
       const { confirmed } = await prompts({
