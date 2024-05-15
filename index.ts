53e8982a67b1eb4d09ab6d5d853398fecbc64d90
murderteeth <89237203+murderteeth@users.noreply.github.com>
2024-05-15 18:22:16 -0400

    Upgrade to gpt 4o (#1)
    * upgrade to gpt 4 omni
    * port to bun
    * simplify indexts

--- index.ts
+++ index.ts
@@ -1,32 +1,30 @@
 #!/usr/bin/env node
 
 import 'dotenv/config'
-import * as readline from 'readline'
-import { STRONGEST_MODEL, next_message } from './ai'
+import yargs from 'yargs/yargs'
+import { hideBin } from 'yargs/helpers'
+import { input, confirm } from '@inquirer/prompts'
 import { oneLine } from 'common-tags'
-import { Spinner } from 'cli-spinner'
+import { STRONGEST_MODEL, next_message } from './ai'
+import chalk from 'chalk'
 import { exec } from 'child_process'
 import * as util from 'util'
-import chalk from 'chalk'
-import prompts from 'prompts'
 
 const execPromise = util.promisify(exec)
-
-const exitCommands = ['quit', 'q', 'exit']
 
 const ai_prompts = {
   bash_system: oneLine`you are a savant at bash commands.
 you only know bash commands, you don't know anything else.
 translate USER's input into a bash command.
+the bash command will be run directly on the system, never include extra information or markup.
 if you're not sure how to translate USER's input, say "IDK".
 you can only say bash commands or "IDK".`,
 
-  security_system: oneLine`you are a savant at linux security
+  security_system: oneLine`you are a savant at linux security. 
 you only know linux security, you don't know anything else.
 USER will input a bash command.
-evaluate USER's command for security threats.
-report the results of your security check by calling report_security_check.
-you can only respond in json format.
+evaluate USER's bash command for security threats.
+you respond in raw json format with no extra markup or surrounding annotation.
 
 FORMAT:
 {
@@ -35,12 +33,6 @@ FORMAT:
 }
 `,
 
-  debug_system: oneLine`you are a savant at console error messages.
-you only know console error messages, you don't know anything else.
-USER will input a console error message.
-evaluate USER's error message and respond with the most likely root cause.
-if you're not sure what the root cause of the error is, say "IDK".
-`
 }
 
 interface SecurityCheck {
@@ -63,107 +55,68 @@ async function securityCheck(input: string) {
   return JSON.parse(result) as SecurityCheck
 }
 
-async function debugCheck(input: string) {
-  return await next_message([
-    { role: 'system', content: ai_prompts.debug_system },
-    { role: 'user', content: input }
-  ], null, STRONGEST_MODEL, 0)
+async function main() {
+  const ora = await (await import('ora')).default
+
+  const argv = await yargs(hideBin(process.argv)).parse()
+  let user_prompt = ''
+
+  if (!argv._.length) {
+    user_prompt = await input({ message: 'Howdy, how can I help? (ctrl-c to quit)' })
+  } else {
+    user_prompt = argv._.join(' ')
+  }
+
+  console.log()
+  const bashSpinner = ora(`"${user_prompt}"`)
+  bashSpinner.start()
+  const bash = await toBash(user_prompt) as string
+  const check = await securityCheck(bash)
+
+  if (check.severity === 'critical') {
+    bashSpinner.fail(`"${user_prompt}"`)
+    bashSpinner.stop()
+
+    console.log()
+    console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
+    console.log(chalk.red(check.summary))
+    console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
+    console.log()
+    process.exit()
+  }
+
+  bashSpinner.succeed(`"${user_prompt}"`)
+  bashSpinner.stop()
+
+  if (check.severity !== 'none') {
+    console.log()
+    console.log(chalk.yellow(`THREAT LEVEL: ${check.severity}`))
+    console.log(chalk.yellow(check.summary))
+    console.log()
+  }
+
+  console.log()
+  console.log(chalk.black.bgCyan(bash))
+  console.log()
+
+  const runSpinner = ora(`"${user_prompt}"`)
+  const confirmed = await confirm({ message: 'Run this command?', default: check.severity === 'none' });
+  if (confirmed) {
+    runSpinner.start()
+    runSpinner.stop()
+
+    const { stdout } = await execPromise(`${bash}`)
+    console.log(stdout)
+
+  } else {
+    runSpinner.fail(`"${bash}"`)
+  }
+
+  console.log()
 }
 
-(async () => {
-  const commandline = process.argv.slice(2).join(' ')
+main().catch((error) => {
+  console.error('Error in main():', error)
+  process.exit(1)
+})
 
-  while(true) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
-
-    if(commandline) {
-      console.log(chalk.green(`rash> ${commandline}`))
-    } else {
-      rl.setPrompt('rash> ')
-      rl.prompt()
-    }
-
-    const line = commandline || await new Promise<string>(resolve => rl.once('line', resolve))
-    if (line === '') {
-      rl.close()
-      continue
-    } else if (exitCommands.includes(line.toLowerCase())) {
-      rl.close()
-      process.exit()
-    } else {
-      const spinner = new Spinner()
-      spinner.setSpinnerString(14)
-      spinner.start()
-      const bash = await toBash(line) as string
-      const check = await securityCheck(bash)
-      spinner.stop(true)
-
-      if(bash === 'IDK') {
-        console.log(chalk.green('Aint that some 💩 try again'))
-        rl.close()
-        continue
-      }
-
-      if(check.severity === 'critical') {
-        console.log()
-        console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
-        console.log(chalk.red(check.summary))
-        console.log(chalk.red('⚠️  CRITICAL THREAT ⚠️'))
-        console.log()
-        rl.close()
-        continue
-      }
-
-      if(check.severity !== 'none') {
-        console.log()
-        console.log(chalk.yellow(`THREAT LEVEL: ${check.severity}`))
-        console.log(chalk.yellow(check.summary))
-        console.log()
-      }
-
-      const { confirmed } = await prompts({
-        type: 'toggle',
-        name: 'confirmed',
-        message: bash,
-        initial: false,
-        active: 'Yes',
-        inactive: 'No'
-      }, {
-        onCancel: () => {
-          rl.close()
-          process.exit()
-        }
-      })
-
-      rl.close()
-
-      if(confirmed) {
-        try {
-          const { stdout } = await execPromise(bash)
-          console.log()
-          console.log(chalk.black.bgCyan(bash))
-          console.log()
-          console.log(stdout)
-          console.log()
-        } catch(error) {
-          const spinner = new Spinner()
-          spinner.setSpinnerString(14)
-          spinner.start()
-          const debug = await debugCheck((error as any).toString())
-          spinner.stop(true)
-
-          console.log()
-          console.error(chalk.red(error))
-          console.log('-----------------------------')
-          console.error(chalk.red(debug))
-          console.log()
-        }
-      }
-
-      if(commandline) process.exit()
-    }
-  }
-})()
